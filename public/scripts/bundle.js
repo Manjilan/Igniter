6fcd64178d59ad47b8b3ce5439303c530c03fdd9
Manjilan <nakarmimanjila@yahoo.com>
2018-11-03 00:33:02 -0700

    added Animations

--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -8,3 +8,12 @@ $(document).ready(function(){
     $('#thankyou').text("Thank you for your Message");
   })
 })
+$(window).on('scroll', function () {
+  console.log(window.scrollY);
+  if(window.scrollY > 975){
+    $('.statistics div h3').addClass('zoomIn');
+  }
+  if(window.scrollY > 1246){
+    $('.checkerboard').addClass('fadeIn');
+  }
+});
